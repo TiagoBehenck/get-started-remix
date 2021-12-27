3917d0830d3fa7591419f6a97b1421699882b174
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2021-12-27 09:05:07 -0300

    first commit

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,24 @@
+import { Link, LoaderFunction, useLoaderData } from "remix";
+import { getPosts } from "~/post";
+import type { Post } from "~/post";
+
+export const loader: LoaderFunction = () => {
+  return getPosts()
+};
+
+export default function Posts() {
+  const posts = useLoaderData<Post[]>();
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map(({ slug, title }) => (
+          <li key={slug}>
+            <Link to={slug}>{title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
